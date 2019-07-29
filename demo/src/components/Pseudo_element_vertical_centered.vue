01ef82b698bb81415648102cd721fc99a9fbfc71
ThomasNg <jielin.wu@ucarinc.com>
2019-07-29 11:29:05 +0800

    no message

---
+++ demo/src/components/Pseudo_element_vertical_centered.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="content">
+    <div class="box"></div>
+  </div>
+</template>
+
+<style lang="less">
+.content {
+  height: 200px;
+  width: 200px;
+  background: skyblue;
+  &::after {
+    content: '';
+    display: inline-block;
+    height: 100%;
+    width: 1px;
+    vertical-align: middle;
+  }
+}
+
+.box {
+  display: inline-block;
+  height: 100px;
+  width: 100px;
+  background: green;
+  vertical-align: middle;
+  align-content: center;
+}
+</style>
---
+++ demo/src/components/thos_praticeList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="pratice_list">
+    <ul>
+      <li>
+        <router-link to="pratice1">
+          伪元素垂直水平居中
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="less">
+.pratice_list {
+  align-content: center;
+  background: green;
+}
+</style>
+
